6fd7a450f32c4992973f146fc81d79088d822737
cutedog690 <cutedog690@outlook.com>
2021-10-31 18:02:20 +0530

    create route to send json response

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  let testJsonObject = {
+    error: false,
+    message: 'None',
+    body:
+      [
+        {
+          id: 10,
+          tag: 'Alpha'
+        },
+        {
+          id: 20,
+          tag: 'Beta'
+        }
+      ],
+  };
+  res.status(200).json(testJsonObject);
 });
 
 module.exports = router;
